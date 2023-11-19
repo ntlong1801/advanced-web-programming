f1d62f4ddd257b0dd763f205dc0b842a718ae0f6
ntlong1801 <ntlongcbl@gmail.com>
2023-11-19 23:37:37 +0700

    revise changPassword page

--- src/pages/ChangePassword/index.jsx
+++ src/pages/ChangePassword/index.jsx
@@ -2,19 +2,23 @@ import TextInput from 'components/FormControl/TextInput';
 import { useForm } from 'react-hook-form';
 import Header from 'layout/header';
 import { Button } from 'primereact/button';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { checkChangeProfile } from 'pages/validation';
 import { yupResolver } from '@hookform/resolvers/yup';
 import instance from 'config';
 import { Toast } from 'primereact/toast';
+import { useNavigate } from 'react-router';
 
 export default function UserPage() {
+  const navigate = useNavigate();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user_profile')));
+  const [errorSamePassword, setErrorSamePassword] = useState(false);
   const toast = useRef(null);
 
   const {
     handleSubmit,
     control,
+    reset,
     formState: {
       errors
     }
@@ -28,20 +32,36 @@ export default function UserPage() {
     toast.current.show({ severity: 'error', summary: 'Fail', detail: msg, life: 3000 });
   };
   const onSubmit = async (data) => {
-    const response = await instance.post('users/changePassword', {
-      username: user?.username,
-      ...data
-    });
-
-    if (response.data?.msg) {
-      showError(response.data?.msg);
+    if (data?.newpassword !== data?.renewpassword) {
+      setErrorSamePassword(true);
     } else {
-      showSuccess('Change password successful');
-    }
+      setErrorSamePassword(false);
+      try {
+        const response = await instance.post('users/changePassword', {
+          username: user?.username,
+          ...data
+        });
 
-    localStorage.setItem('user_profile', JSON.stringify(response.data));
-    setUser(response.data);
+        if (response.data?.msg) {
+          showError(response.data?.msg);
+        } else {
+          reset({}, { keepValues: false });
+          showSuccess('Change password successful');
+        }
+      } catch (error) {
+        if (error.response?.data?.message === 'Unauthorized') {
+          navigate('/signin');
+        }
+      }
+    }
   };
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user_profile')));
+    if (!user) {
+      navigate('/signin');
+    }
+  }, []);
 
   return (
     <div className="background" style={{ display: 'flex', flexDirection: 'column' }}>
@@ -50,7 +70,7 @@ export default function UserPage() {
       <div className="flex align-items-center justify-content-center" style={{ flex: 1 }}>
         <div
           className="surface-card p-4 shadow-2 border-round w-full lg:w-6"
-          style={{ maxWidth: '400px', height: '50vh' }}
+          style={{ maxWidth: '400px' }}
         >
           <form autoComplete="off" onSubmit={handleSubmit(onSubmit)} className="p-fluid justify-content-center">
             <div className="text-center">
@@ -68,7 +88,6 @@ export default function UserPage() {
             <TextInput
               type="password"
               name="newpassword"
-              autoFocus
               control={control}
               errors={errors}
               label="New password"
@@ -77,12 +96,12 @@ export default function UserPage() {
             <TextInput
               type="password"
               name="renewpassword"
-              autoFocus
               control={control}
               errors={errors}
               label="Re-New password"
               defaultValue=""
             />
+            {errorSamePassword && <span className="text-red-500">Re-new password must be the same as new password</span>}
             <div className="text-center mt-4">
               <Button
                 label="Change"
