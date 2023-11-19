518e02f8acc1939bed614699c58416b8cc2f25f1
ntlong1801 <ntlongcbl@gmail.com>
2023-11-19 20:59:26 +0700

    revise orn config with .env

--- dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    findOne(username: string): Promise<import("./user.entity").User | {
-        message: string;
-    }>;
-    changeProfile(changeProfileDTO: Record<string, any>): Promise<import("./user.entity").User>;
-}
--- dist/user/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class UserService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    findByUsername(username: string): Promise<User | undefined>;
-    createUser(username: string, password: string, fullName: string, email: string): Promise<User>;
-    updateUser(username: string, fullname: string, email: string, password: string | null): Promise<User>;
-}
