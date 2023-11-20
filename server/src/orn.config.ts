a3f292884d6c7ba0a689ef326a17e740fdc669f3
ntlong1801 <ntlongcbl@gmail.com>
2023-11-20 08:36:56 +0700

    [server] change using .env for orn config and port

--- server/src/orn.config.ts
+++ server/src/orn.config.ts
@@ -1,12 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+const db_options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) ?? 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  port: 5432,
-  host: 'localhost',
-  username: 'postgres',
-  password: '123',
-  database: 'AWP_BE',
+  ...db_options,
   synchronize: true,
   entities: [User],
 };
